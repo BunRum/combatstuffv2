165dcec4ec82d8438993c8cd279a7cf92d8e7030
BunRum <adamventura4@gmail.com>
2025-03-28 15:42:20 -0700

    1

--- src/shared/combat/entity/combatClass.ts
+++ src/shared/combat/entity/combatClass.ts
@@ -89,7 +89,7 @@ export class combatClassBase {
         } // n()
         this.Keybinds.set(input, middle);
         print("possessed")
-        print("soa")
+        print("soaa")
         this.bindInputs();
     }
 

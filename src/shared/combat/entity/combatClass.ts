f46caf09709514e4622a938091581c1812f88733
melodi <bunrumz@>
2025-03-28 15:22:11 -0700

    1

--- src/shared/combat/entity/combatClass.ts
+++ src/shared/combat/entity/combatClass.ts
@@ -88,6 +88,7 @@ export class combatClassBase {
             func(state)
         } // n()
         this.Keybinds.set(input, middle);
+        print("possessed")
         this.bindInputs();
     }
 
